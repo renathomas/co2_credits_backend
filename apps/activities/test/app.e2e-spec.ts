757f2f039efee8cbb2d39b660f7c11a16a3c03c9
Rena Thomas <crenathomas@gmail.com>
2023-12-03 14:16:26 +0200

    added resource to manage user's eco activities inside the app

--- apps/activities/test/app.e2e-spec.ts
+++ apps/activities/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { ActivitiesModule } from './../src/activities.module';
 
-describe('AppController (e2e)', () => {
+describe('ActivitiesController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [ActivitiesModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
